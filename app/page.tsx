3d1eedfc88522c73093f5b52eb92638a7656818e
Damilola Adebowale <adebowaletemitayo40@gmail.com>
2024-07-28 13:47:47 +0100

    Implemented the homepage, servicepage, resumepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { FiDownload } from "react-icons/fi";
 import React from "react";
 import Socials from "@/components/Socials";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 type Props = {};
 
@@ -13,7 +14,7 @@ const Home = (props: Props) => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left order-2 xl:order-none">
+          <div className="text-center xl:text-left order-4 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I&apos;m <br />
@@ -47,6 +48,8 @@ const Home = (props: Props) => {
           </div>
         </div>
       </div>
+
+      <Stats />
     </section>
   );
 };
